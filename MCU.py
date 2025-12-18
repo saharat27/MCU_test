0df39b12234414a5d74912b7644a0a44f8ae2fea
saharat27 <saharatgw@gmail.com>
2025-12-19 01:22:15 +0700

    Update MCU.py for enhanced serial communication
    Refactor MCU.py to improve data reading and file handling.

--- MCU.py
+++ MCU.py
@@ -1,36 +1,56 @@
 import serial
 import time
 
-PORT = 'COM7'
-BAUD = 9600
-FILENAME = r'Z:\Common Data for Line\ICT\Trainee\Kaorad\python\MCU\text\data1.txt'
+PORT = 'COM3'
+BAUD = 115200
+ROUNDS = 3
+FILENAME = r'C:\Users\VICTUS\Downloads\MCU\data\data1.txt'
 
 ser = serial.Serial(PORT, BAUD, timeout=1)
 time.sleep(2)
 
+def read_block(cmd):
+    buffer = []
+    ser.write(cmd.encode())
+    time.sleep(0.1)
+
+    while True:
+        line = ser.readline()
+        if line:
+            value = line.decode('utf-8', errors='ignore').strip()
+            print('Read:', value)
+            buffer.append(value)
+
+            if "End<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" in value:
+                break
+    return buffer
+
+
 try:
-    with open(FILENAME, 'w+') as file:
-        for round_num in range(1, 4):  
-            print(f"{round_num}")
-            file.write(f"--- Round {round_num} ---\n") 
+    last_c_data = []
+    last_d_data = []
 
-            while True:
-                ser.write(b'c')  
-                line = ser.readline()
-                if line:
-                    value = line.decode('utf-8', errors='ignore').strip()
-                    print('Read:', value)
-                    file.write(value + '\n')
-        
-                    if "End<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" in value:
-                        print(f"พบ End! จบรอบที่ {round_num}\n")
-                        file.write("\n")  
-                        break
-                else:
-                    print('No data')
 
-                time.sleep(0.1)
+    for i in range(1, ROUNDS + 1):
+        print(f"\nread c round {i}")
+        data = read_block('c')
+        if i == ROUNDS:
+            last_c_data = data  
+
+
+    for i in range(1, ROUNDS + 1):
+        print(f"\nread d round{i}")
+        data = read_block('d')
+        if i == ROUNDS:
+            last_d_data = data  
+
+
+    with open(FILENAME, 'w', encoding='utf-8') as file:
+        file.write("===== LAST ROUND : C =====\n")
+        file.write("\n".join(last_c_data))
+        file.write("\n\n===== LAST ROUND : D =====\n")
+        file.write("\n".join(last_d_data))
 
 finally:
     ser.close()
-    print('Saved to', FILENAME)
+    print("\nSaved to", FILENAME)
